1b44b3477a09ccb81e17666588ed9692e0197fd0
mittelmabigkeit <comp422012016@mail.ru>
2016-09-08 16:56:56 +0300

    php

---
+++ index.php
@@ -0,0 +1,20 @@
+<meta charset="Utf-8">
+<?php
+$example=33;
+    echo $example;
+    echo "</br>";
+ if (is_integer($example))
+    echo "yes";
+    echo "</br>";
+ if (isset($example))
+    echo "yes";
+    echo "</br>";
+ if (is_double($example))
+    echo "yes";
+    echo "</br>";
+$taptap="тест";
+    echo $taptap;
+    echo "</br>";
+ if (isset($taptap))
+    echo "yes";
+?> 
