fe171fe6b79c3c5abe27f8fdfa2bc352ff9f2d13
mittelmabigkeit <comp422012016@mail.ru>
2016-09-08 17:29:59 +0300

    phpone

--- index.php
+++ index.php
@@ -15,6 +15,38 @@ $example=33;
 $taptap="тест";
     echo $taptap;
     echo "</br>";
- if (isset($taptap))
+ if (isset($taptap)) {
     echo "yes";
+    }
+    else  {
+       echo "</br>";
+       echo "no";
+       }
+       echo "</br>";
+$MyArray=array(
+         1 => "a",
+         2 => "b",
+         3 => "c",
+         4 => "d",
+	);
+echo "<pre>";
+print_r($MyArray);
+       echo "</br>";
+var_dump($MyArray);
+       echo "</br>";
+var_export($MyArray);
+       echo "</br>";
+       echo "</pre>";
+
+define("PI","3,14",true);
+       echo PI;
+       echo "</br>";
+
+       echo "</br>";
+       echo __FILE__;
+       echo "</br>";
+       echo __LINE__;
+       echo "</br>";
+       echo PHP_VERSION;
+
 ?> 
